f145d8c7e423c8e41c6d9b915fe73c62c88b3df7
Zeb <zabdeilmercado@gmail.com>
2025-04-06 20:13:58 +0800

    edited something

--- src/router/index.js
+++ src/router/index.js
@@ -2,21 +2,22 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
 import RegisterView from '@/views/auth/RegisterView.vue'
 
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/',
+    name: 'register',
+    component: RegisterView,
+  },
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'login',
-      component: LoginView
-    },
-    {
-      path: '/',
-      name: 'register',
-      component: RegisterView
-    },
-  ]
+  history: createWebHistory(),
+  routes,
 })
 
 export default router
